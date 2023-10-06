e9a89c1bd14bb2254ebc5f564e1f2755aa1fe1e8
SamroodAli <samrood@peoplegrove.com>
2023-10-06 22:36:11 +0530

    feat: add code to read search string and filename

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> =  env::args().collect();
+
+    let Some(search_string) = args.get(1) else {
+        return eprintln!("Please provide a search string as the first argument");
+    };
+
+    let Some(file_path) = args.get(2) else {
+        return eprintln!("Please provide a filename as the second argument");
+    };
+
+    let Ok(content) = fs::read_to_string(file_path) else {
+        return eprintln!("Error parsing the file");
+    };
+
+    println!("{content}");
 }
